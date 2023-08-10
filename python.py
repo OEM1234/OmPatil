f895b6d4021f2eb0d33aee7a798ef642fc9510d2
OEM1234 <omcp21hite@student.mes.ac.in>
2023-08-10 11:05:38 +0530

    adding files

---
+++ python.py
@@ -0,0 +1,70 @@
+# a = input("enter the value")
+# a = int(a)
+# if  a > 10:
+#     print("greater")
+# else:
+#     print("not greater than 10")
+
+# a = 3
+# if a == 1:
+#  print("one")
+# elif a == 2:
+#  print("two")
+# elif a == 3:
+#  print("three")
+
+# a = 1
+# b = 2
+# c = 3
+#  if a<b and a<c :
+# elif b<c 
+# print(b)
+
+# for i in range string , list ,tuple 
+# for i in range (12)
+# print(i,=,'')
+
+
+# for i in range (4):
+#   for j in range (5):
+#     print(i,j,end=',')
+
+# while 0:
+#     print("hello")
+# a = 0
+# while a < 5:
+#     print(a)
+#     a+=1 
+
+# for i in range (10):
+#     if i == 5:
+#         continue
+#   print(i)
+
+# for i in range (10):
+#     if i == 5:
+#         break
+#     print(i)
+
+
+# def sum(a,b):
+#     # print("value=",a*b)
+#     return a+b
+
+# a = sum(5,5)
+# print(a)
+
+# x = lambda x,y: x+y
+# print(x(5,3))
+
+# def sum(a,b = 1):
+#   print(a,b)
+
+# sum(5)
+
+
+# def sum(**a):
+#   print(type(a))
+
+#   print(a['b'])
+# sum(a = 1,b = 2, c = 3, d = 4 , e = 5)
